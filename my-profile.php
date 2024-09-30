39d42e515b2044d08bcbb01424768b91a9a541b5
codeparagon <mirzaahmar1@gmail.com>
2024-09-30 17:30:39 +0500

    fix Container class issue in my-profile css

--- my-profile.php
+++ my-profile.php
@@ -7,7 +7,7 @@ include './header.php';
  <!-- fontawesome -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css" integrity="sha512-q3eWabyZPc1XTCmF+8/LuE1ozpg5xxn7iO89yfSOd5/oKvyqLngoNGsx8jq92Y8eXJ/IRxQbEC+FGSYxtk2oiw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-<div class="container">
+<div class="mp-container">
   <div class="box">
    
     <div class="my-box-card">
